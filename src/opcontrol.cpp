e4926ee31779917e51c5477560f330d1fa038fd0
Joseph Spencer <spencerj21@chcstudent.com>
2019-02-05 17:48:33 -0500

    Impl opcontrol()

--- src/opcontrol.cpp
+++ src/opcontrol.cpp
@@ -1,4 +1,5 @@
 #include "main.h"
+#include "robot.hpp"
 
 /**
  * Runs the operator control code. This function will be started in its own task
@@ -14,5 +15,39 @@
  * task, not resume it from where it left off.
  */
 void opcontrol() {
+	robot::catapult.tarePosition();
 	
+	while(true){
+    robot::chassis.tank(
+      robot::controller.getAnalog(okapi::ControllerAnalog::leftY) * (robot::controller.getDigital(okapi::ControllerDigital::R1) ? 0.6 : 1.0),
+      robot::controller.getAnalog(okapi::ControllerAnalog::rightY) * (robot::controller.getDigital(okapi::ControllerDigital::R1) ? 0.6 : 1.0));
+
+		if(robot::controller.getDigital(okapi::ControllerDigital::X) || robot::catapultLimit.changedToReleased()){
+			robot::catapult.tarePosition();
+		}
+
+    if(robot::controller.getDigital(okapi::ControllerDigital::R2) || robot::catapultLimit.isPressed()){
+      robot::catapult.moveAbsolute(robot::firingCatapultPosition, 200);
+    }else{
+      robot::catapult.moveAbsolute(robot::primedCatapultPosition, 200);
+    }
+
+    if(robot::controller.getDigital(okapi::ControllerDigital::R1)){
+      robot::intake.moveVoltage(12000);
+    }else if(robot::controller.getDigital(okapi::ControllerDigital::A)){
+			robot::intake.moveVoltage(-12000);
+		}else{
+      robot::intake.moveVoltage(0);
+    }
+
+		if(robot::controller.getDigital(okapi::ControllerDigital::L1)){
+      robot::scraper.moveVelocity(50);
+    }else if(robot::controller.getDigital(okapi::ControllerDigital::L2)){
+			robot::scraper.moveVelocity(-50);
+		}else{
+      robot::scraper.moveVelocity(0);
+    }
+
+		pros::delay(10);
+	}
 }
